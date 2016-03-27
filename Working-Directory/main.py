36773eab552d1c229932ddc3af6ff0e53a3c70e7
bmroach <bmroach23@gmail.com>
2016-03-27 00:03:15 -0400

    update, currently non functional
    Cleaned code up, implemented greater variable usage and less arithmetic
    on constants to allow for parameter differentiation. Currently non
    terminating - not sure where bug is, will continue looking, or Adam
    feel free to make a branch and check it out.

--- Working-Directory/main.py
+++ Working-Directory/main.py
@@ -18,8 +18,6 @@ import copy
 #______________________________________________________________________________
 #Start main.py
 
-
-#
 # Global parameters
 dirIn = "../Original-Audio-Samples/"
 dirOut = "../Output-Audio-Samples/"
@@ -27,8 +25,8 @@ dirOut = "../Output-Audio-Samples/"
 numChannels = 1                      # mono
 sampleWidth = 2                      # in bytes, a 16-bit short
 sampleRate = 44100
+mulFactor = sampleRate * 10
 
-lamb = 1
 
 #Reference Functions
 
@@ -60,7 +58,7 @@ def readWaveFile(fileName,withParams=False,asNumpy=False):
         
 #______________________________________________________________________________
         
-def main(fileName, preDelay = 0, Decay = 0, Variation = 0, presetOn = False, preset = ""):
+def reverb(fileName, preDelay = 0, Decay = 0, Variation = 0, presetOn = False, preset = ""):
     """fileName: name of file in string form
         preDelay: delay befor reverb begins (seconds)
         Decay: hang time of signal (seconds)
@@ -72,56 +70,51 @@ def main(fileName, preDelay = 0, Decay = 0, Variation = 0, presetOn = False, pre
         
     """ Version History
         3/22/16: Prototype framework in progress (current) [BMR]
+        3/25/16: Decay fix [AE]
+        3/26/16: Framework adjustments [BMR]
     """
     
+    #convert from seconds to pre determined unit. ie mulFactor = 10 -> width = .1s
+    preDelay *= mulFactor
+    Decay *= mulFactor
     
-        
+    position = 0
+    windowWidth = sampleRate/mulFactor    
     data = readWaveFile(fileName)
     
     #pad zeros to add room for reverb after initial termination point 
     for x in range(Decay*sampleRate + preDelay *sampleRate):
         data.append(0)
-    
-    position = 0
-    
     seconds = len(data)//sampleRate
     
-    #for every 1 second window
-    for i in range(seconds):
-        #capture current window ##and apply initial decay        
-        currentWindow = data[position:position+sampleRate] #width of 1 second, "cloned sample"
-        for x in range(len(currentWindow)):
-            currentWindow[x] = math.floor(currentWindow[x] * math.e**(-1*(1/Decay))) #begin decay #CHECK        
-        
+    #for every (sR/mF) second window
+    for i in range(seconds*mulFactor):
+        #capture current window        
+        currentWindow = data[position:position+(windowWidth)]  
         
         #length of decay determines reach of cloned signal
         forwardPosition = copy.deepcopy(position) #marks starting sample
-        forwardPosition += (sampleRate + (preDelay * sampleRate) ) #account for delay before reverb starts
         
-#        #_________
-#        #test code
-#        print(/len(currentWindow))
-#        return 
-#        #_________        
-        
+        #account for delay before reverb starts
+        forwardPosition += ((windowWidth) + (preDelay * (windowWidth) )) 
+
         for k in range(Decay):
-            #add each clone sample to samples forward in time           
-            for j in range(sampleRate):
-                if (forwardPosition + j) < len(data):
-                    data[forwardPosition + j] += currentWindow[j]
-#                print(currentWindow[j])
-                                
-                
             #decrease amplitude of currentWindow (reverb decaying over time)
             for x in range(len(currentWindow)):
-                currentWindow[x] = math.floor(currentWindow[x] * math.e**(-1*(1/Decay))) #check               
+                currentWindow[x] = math.floor(currentWindow[x] * math.e**(-1*(k/Decay)))                  
+         
+            #add each clone sample to samples forward in time           
+            for j in range(sampleRate):
+                data[forwardPosition + j] += currentWindow[j]
+#                 if (forwardPosition + j) < len(data):                #testing without these two lines
+#                     data[forwardPosition + j] += currentWindow[j]
+
+            forwardPosition += windowWidth
+        
+        #mark where next window will start
+        position += windowWidth
+         
             
-            forwardPosition += sampleRate
-        
-        #mark where next 1sec window will start
-        position += sampleRate 
-    
-        
     #finally, write out to .wav file
     writeWaveFile(fileName, data)
    
